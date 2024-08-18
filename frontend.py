f4d139bcc36426cf80d378d252f710f2b63a67bb
AdityaVinu <167654821+AdityaVinu@users.noreply.github.com>
2024-08-18 16:06:57 -0700

    Front end. WIP

--- frontend.py
+++ frontend.py
@@ -1,12 +1,22 @@
 from cmu_graphics import *
 import time
+import money
 
 
+app.width = 1500
+app.height = 1000
+app.ucounter = 0
+app.daycounter = 0
+dayLabel = Label("",30,25,size=20)
+balance = Label("",750, 30, size=30)
 
-#p1 = Bank(10000, 0, 0)
-#print(p1.balance)
-app.width = 900
-app.height = 900
+playerBank = money.Bank(100000, 0, 20000) #temporary, will store in a seperate txt file later
+
+def onStep():
+    app.ucounter += 1/30
+    app.daycounter += (1/30)/60
+    dayLabel.value = "day " + str(int(app.daycounter))
+    balance.value = "Current Balance: " +  str(playerBank.balance)
 
 
 cmu_graphics.run()
