1fd2959b7fd43ca34ad5348a7b139ef07151d0a9
AdityaVinu <167654821+AdityaVinu@users.noreply.github.com>
2024-08-18 11:59:30 -0700

    Money system. Contains banking, stocks, and debt systems

--- money.py
+++ money.py
@@ -1,11 +1,11 @@
 import random
-
-class BankAndStocks():
-    def __init__(self, balance, savings, paycheck,investment):
+class Bank():
+    def __init__(self, balance, savings, paycheck):
         self.balance = balance
         self.savings = savings
         self.paycheck = paycheck
-        self.investment = investment
+        self.debtLimit = 3
+        self.debtCounter = 0
     def getInterest(self, interest_rate):
         self.savings = self.savings * (interest_rate + 1)
         return self.savings * (interest_rate + 1)
@@ -14,6 +14,8 @@ class BankAndStocks():
             return "Error: Amount deposited is higher than current balance"
         elif amount < 0:
             return "Error: Cannot deposit/withdraw negative numbers"
+        elif self.balance < 0:
+            return "You are in debt, you don't have anything to put in savings"
         else:
             self.savings +=  amount
             self.balance -= amount
@@ -29,23 +31,41 @@ class BankAndStocks():
     def payCheck(self):
         self.balance += self.paycheck
     def incomeTax(self, tax_rate):
-        self.balance -= tax_rate*self.paycheck
-    def chargeRent(self, rent):
-        self.balance -= rent
-    def fluctuation(self, lowerRate, upperRate):
-        rate = random.choice([lowerRate,upperRate]) #lower rate is the percent decreased, upper rate is percent increased (example: 5% decrease and 10% increase would be 0.95 and 1.1)
-        self.investment = self.investment * rate
-    def invest(self, amount):
-        if amount > self.balance:
-            return "Error: Amount invested is higher than current balance"
-        elif amount < 0:
-            return "Error: Cannot invest/liquidate negative numbers"
+        if (self.balance - tax_rate*self.paycheck < 0):
+            if self.debtCounter < self.debtLimit:
+                self.balance -= tax_rate*self.paycheck
+                self.debtCounter += 1
+                return "You have currently gone more into debt"
+            else:
+                return "You have been in debt for far too long. You lose!"
+        else:
+            self.balance -= tax_rate*self.paycheck
+            return tax_rate*self.paycheck
+    def reduceBalance(self, amount): #can be used to charge rent/fees or can be used to subtract amounts from balance after investing them into stocks
+        if (self.balance - amount < 0):
+            if self.debtCounter < self.debtLimit:
+                self.balance -= amount
+                self.debtCounter += 1
+                return "You have currently gone more into debt"
+            else:
+                return "You have been in debt for far too long. You lose!"
         else:
             self.balance -= amount
-            self.investment += amount
+            return amount
+class Stonks():
+    def __init__(self, decreaseRate, increaseRate):
+        self.decreaseRate = decreaseRate
+        self.increaseRate = increaseRate
+        self.investment = 0
+    def recieveInvestment(self, investment):
+        self.investment += investment
     def liquidate(self):
         if self.investment == 0:
-            return "You don't have anything invested"
+            return "You don't have anything to liquidate"
         else:
-            self.balance += self.investment
+            r = self.investment
             self.investment = 0
+            return r
+    def fluctuation(self):
+        rate = random.choice([self.decreaseRate, self.increaseRate])
+        self.investment = self.investment * rate
